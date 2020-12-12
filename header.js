4903c14470b359bfcd5e0a25d40661b33906fe8c
YongJaeKim <kiyoja07@gmail.com>
2020-12-12 15:46:59 +0900

    add header

---
+++ header.js
@@ -0,0 +1,63 @@
+const hero = document.querySelector('.hero');
+const header = document.querySelector('.header__container');
+const logo = header.querySelector('.header--logo');
+let headerHeightDefault = header.offsetHeight;
+let prevYScroll = window.pageYOffset;
+
+function scrollHandler(event) {
+    let yScroll = window.pageYOffset; // 브라우저 좌상단에서 스크롤된 픽셀
+
+    if (yScroll > headerHeightDefault) {
+        if (yScroll > prevYScroll) {
+            header.classList.add('scrollHeader');
+            header.style.top = headerHeightDefault * -2 + 'px';
+        } else {
+            header.classList.remove('scrollHeader');
+            header.style.top = '0';
+        }
+    } else {
+        header.classList.remove('scrollHeader');
+        header.style.top = '0';
+    } 
+    prevYScroll = yScroll;
+}
+
+function onScroll() {
+    window.addEventListener('scroll', scrollHandler);
+}
+
+function setTopPadding() {
+    hero.style.paddingTop = headerHeightDefault + 'px';
+}
+
+function resizeWindow() {
+    window.addEventListener('resize', setTopPadding);
+}
+
+
+function scrollToTop(event) {
+    // returns the number of pixels an element's content is scrolled vertically
+    // document.body.scrollTop : For Safari
+    // document.documentElement.scrollTop : For Chrome, Firefox, IE and Opera
+    
+    const c = document.documentElement.scrollTop || document.body.scrollTop;
+  
+    if (c > 0) {
+      window.requestAnimationFrame(scrollToTop);
+      window.scrollTo(0, c - c / 10);
+    }
+  }
+
+
+function onClick() {
+    logo.addEventListener('click', scrollToTop);
+}
+
+function init() {
+    setTopPadding();
+    resizeWindow();
+    onScroll();
+    onClick();
+}
+
+init();
